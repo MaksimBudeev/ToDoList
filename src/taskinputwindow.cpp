30f205396cb0eb766748ec746f6da79cc8580b33
Maksim <grebenshicov123@gmail.com>
2021-05-10 01:23:03 +0700

    Added design of dialog window

--- src/taskinputwindow.cpp
+++ src/taskinputwindow.cpp
@@ -1,7 +1,33 @@
 #include "taskinputwindow.h"
 
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QComboBox>
+#include <QFormLayout>
+#include <QStringList>
+#include <QDialogButtonBox>
+
 TaskInputDialog::TaskInputDialog(QWidget *parent)
     : QDialog(parent)
 {
-    setMinimumSize(300, 200);
+    setFixedSize(300, 200);
+
+    taskLineEdit = new QLineEdit(this);
+    dateLineEdit = new QLineEdit(this);
+    priorityComboBox = new QComboBox(this);
+
+    QStringList priorityList;
+    priorityList << "1" << "2" << "3" << "4" << "5";
+    priorityComboBox->addItems(priorityList);
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+
+    QFormLayout* dialogLayout = new QFormLayout();
+    dialogLayout->addRow("Task:", taskLineEdit);
+    dialogLayout->addRow("Date:", dateLineEdit);
+    dialogLayout->addRow("Priority:", priorityComboBox);
+    dialogLayout->addRow("", buttonBox);
+
+    setLayout(dialogLayout);
 }
--- src/taskinputwindow.h
+++ src/taskinputwindow.h
@@ -2,9 +2,18 @@
 
 #include <QDialog>
 
+class QComboBox;
+class QLineEdit;
+class QFormLayout;
+
 class TaskInputDialog: public QDialog
 {
     Q_OBJECT
 public:
     TaskInputDialog(QWidget *parent = nullptr);
+
+private:
+    QLineEdit* taskLineEdit = nullptr;
+    QLineEdit* dateLineEdit = nullptr;
+    QComboBox* priorityComboBox = nullptr;
 };
