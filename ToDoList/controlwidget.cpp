02bededd5eccb1309f4a61b3b409fcef6938a2c9
Maksim <grebenshicov123@gmail.com>
2021-05-09 22:30:12 +0700

    add layout and buttons

--- ToDoList/controlwidget.cpp
+++ ToDoList/controlwidget.cpp
@@ -6,5 +6,18 @@
 ControlWidget::ControlWidget(QWidget *parent)
     : QWidget(parent)
 {
+    QVBoxLayout *vbox = new QVBoxLayout(this);
+
+    QPushButton *newTask = new QPushButton("Create new task", this);
+    QPushButton *deleteTask = new QPushButton("Delete task", this);
+    QPushButton *compliteTask = new QPushButton("Complite task", this);
+    QPushButton *editTask = new QPushButton("Edit task", this);
+
+    vbox->addWidget(newTask);
+    vbox->addWidget(deleteTask);
+    vbox->addWidget(compliteTask);
+    vbox->addWidget(editTask);
+
+    setLayout(vbox);
 
 }
