432a8123de31233ca981b8053bb0139af3f37d78
Balabasek <studentkov@mail.ru>
2021-05-18 14:13:12 +0700

    Disabled namespace std in header file

--- src/Task.h
+++ src/Task.h
@@ -3,41 +3,40 @@
 #include <string>
 #include <vector>
 
-using namespace std;
 
 class Task {
 private:
-    string date;
-    string text;
+    std::string date;
+    std::string text;
     int priority; //Приоритет задачи от 1 до 5
 public:
     // Конструктор:
-    Task(string date, string text, int priority);
+    Task(std::string date, std::string text, int priority);
     Task();
     // Установить дату:
-    void SetDate(string newDate);
+    void SetDate(std::string newDate);
     // Установить текст:
-    void SetText(string newText);
+    void SetText(std::string newText);
     // Установить приоритет:
     void SetPriority(int newPriority);
     // Вернуть значение даты:
-    string GetDate()const;
+    std::string GetDate()const;
     // Вернуть значение текста:
-    string GetText()const;
+    std::string GetText()const;
     // Вернуть значение приоритета:
     int GetPriority()const;
     bool operator == (const Task & b);
     bool operator != (const Task & b);
 };
 
-void DeleteTask(vector<Task>& vector, string date, int priority, string text);
+void DeleteTask(std::vector<Task>& vector, std::string date, int priority, std::string text);
 // добавление заметки:
-void SetTaskForDate(vector<Task>& taskVector, string newDate, int newPriority, string newText);
+void SetTaskForDate(std::vector<Task>& taskVector, std::string newDate, int newPriority, std::string newText);
 // вывод заметок на данную дату в консоль(временно):
-void GetTaskForDate(vector<Task> taskVector, string date);
+void GetTaskForDate(std::vector<Task> taskVector, std::string date);
 // изменение даты:
-void ChangeTaskDate(vector<Task>& taskVector, string date, int priority, string text, string newDate);
+void ChangeTaskDate(std::vector<Task>& taskVector, std::string date, int priority, std::string text, std::string newDate);
 // изменение текста заметки:
-void ChangeTaskText(vector<Task>& taskVector, string date, int priority, string text, string newText);
+void ChangeTaskText(std::vector<Task>& taskVector, std::string date, int priority, std::string text, std::string newText);
 // изменение приоритета заметки:
-void ChangeTaskPriority(vector<Task>& taskVector, string date, int priority, string text, int newPriority);
+void ChangeTaskPriority(std::vector<Task>& taskVector, std::string date, int priority, std::string text, int newPriority);
