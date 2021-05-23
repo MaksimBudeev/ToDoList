782af11d09b85d43eda4e7e4409f06ce576acdec
Meghan-Murray <aa.mironets@gmail.com>
2021-05-23 18:07:44 +0700

    Disabled namespace std in DataInput

--- src/DataInput.cpp
+++ src/DataInput.cpp
@@ -1,25 +1,26 @@
 #include "DataInput.h"
 #include "Task.h"
 
-void setTaskInFile(vector<Task>& taskVector, string date, int priority, string text)
+
+void setTaskInFile(std::vector<Task>& taskVector, std::string date, int priority, std::string text)
 {
-    fstream storage("storage.txt", ios::app);
-        storage << date << " " << priority << " " << text << endl;
+    std::fstream storage("storage.txt", std::ios::app);
+        storage << date << " " << priority << " " << text << std::endl;
     setTaskForDate(taskVector, date, priority, text);
     storage.close();
 }
 
-void changeStringInFile(vector<Task>& taskVector, string date_p, int priority_p, string text_p, string newString, int typeString)
+void changeStringInFile(std::vector<Task>& taskVector, std::string date_p, int priority_p, std::string text_p, std::string newString, int typeString)
 {
     int readChar = 0;
     int charCount = 0;
-    string date;
-    string prior;
-    string text;
+    std::string date;
+    std::string prior;
+    std::string text;
     int priority;
 
-    fstream storage("storage.txt", ios::in);
-    fstream cache("cache.txt", ios::out);          
+    std::fstream storage("storage.txt", std::ios::in);
+    std::fstream cache("cache.txt", std::ios::out);          
 
     while ((readChar = storage.get()) != EOF) {
         if (char(readChar) != '\n') {
@@ -41,23 +42,23 @@ void changeStringInFile(vector<Task>& taskVector, string date_p, int priority_p,
             if ((date == date_p) && (priority == priority_p) && (text == text_p)) {
                 if (typeString == 1) {
                     changeTaskDate(taskVector, date_p, priority_p, text_p, newString);
-                    cache << newString << " " << priority_p << " " << text_p << endl;
+                    cache << newString << " " << priority_p << " " << text_p << std::endl;
                 }
                 if (typeString == 2) {
                     changeTaskText(taskVector, date_p, priority_p, text_p, newString);
-                    cache << date_p << " " << priority_p << " " << newString << endl;
+                    cache << date_p << " " << priority_p << " " << newString << std::endl;
                 }
                 if (typeString == 3) {
                     int newPriority = stoi(newString);
                     changeTaskPriority(taskVector, date_p, priority_p, text_p, newPriority);
-                    cache << date_p << " " << newPriority << " " << text_p << endl;
+                    cache << date_p << " " << newPriority << " " << text_p << std::endl;
                 }
                 if (typeString == 4) {
                     deleteTask(taskVector, date_p, priority_p, text_p);
                 }
 
             } else {
-               cache << date << " " << priority << " " << text << endl;
+               cache << date << " " << priority << " " << text << std::endl;
                 if (typeString == 0) {
                     setTaskForDate(taskVector, date, priority, text);
                 }
@@ -72,8 +73,8 @@ void changeStringInFile(vector<Task>& taskVector, string date_p, int priority_p,
     storage.close();
     cache.close();
 
-    cache.open("cache.txt", ios::in);
-    storage.open("storage.txt", ios::out);
+    cache.open("cache.txt", std::ios::in);
+    storage.open("storage.txt", std::ios::out);
 
     while ((readChar = cache.get()) != EOF) {
         if (char(readChar) != '\n') {
@@ -92,7 +93,7 @@ void changeStringInFile(vector<Task>& taskVector, string date_p, int priority_p,
                 }
             }
         } else {
-            storage << date << " " << priority << " " << text << endl;
+            storage << date << " " << priority << " " << text << std::endl;
             charCount = 0;
             date.clear();
             prior.clear();
@@ -104,29 +105,29 @@ void changeStringInFile(vector<Task>& taskVector, string date_p, int priority_p,
     cache.close(); 
 }
 
-void writeInVector(vector<Task>& taskVector)
+void writeInVector(std::vector<Task>& taskVector)
 {
     int typeString = 0;
     changeStringInFile(taskVector, "", 0, "", "", typeString);
 }
 
-void changeDateInFile(vector<Task>& taskVector, string date, int priority, string text, string newDate)
+void changeDateInFile(std::vector<Task>& taskVector, std::string date, int priority, std::string text, std::string newDate)
 {
     int typeString = 1;
     changeStringInFile(taskVector, date, priority, text, newDate, typeString);
 }
-void changeTextInFile(vector<Task>& taskVector, string date, int priority, string text, string newText)
+void changeTextInFile(std::vector<Task>& taskVector, std::string date, int priority, std::string text, std::string newText)
 {
     int typeString = 2;
     changeStringInFile(taskVector, date, priority, text, newText, typeString);
 }
-void changePriorityInFile(vector<Task>& taskVector, string date, int priority, string text, int newPriority)
+void changePriorityInFile(std::vector<Task>& taskVector, std::string date, int priority, std::string text, int newPriority)
 {
     int typeString = 3;
-    string prior = to_string(newPriority);
+    std::string prior = std::to_string(newPriority);
     changeStringInFile(taskVector, date, priority, text, prior, typeString);
 }
-void deleteTaskInFile(vector<Task>& taskVector, string date, int priority, string text)
+void deleteTaskInFile(std::vector<Task>& taskVector, std::string date, int priority, std::string text)
 {
     int typeString = 4;
     changeStringInFile(taskVector, date, priority, text, "", typeString);
--- src/DataInput.h
+++ src/DataInput.h
@@ -6,13 +6,21 @@
 #include <vector>
 #include "Task.h"
 
-using namespace std;
+void writeInVector(std::vector<Task>& taskVector);
+void setTaskInFile(std::vector<Task>& taskVector, std::string date, int priority, std::string text);
 
-void writeInVector(vector<Task>& taskVector);
-void setTaskInFile(vector<Task>& taskVector, string date, int priority, string text);
-void changePriorityInFile(vector<Task>& taskVector, string date, int priority, string text, int newPriority);
-void changeStringInFile(vector<Task>& taskVector, string date_p, int priority_p, string text_p, string newString, int typeString);
-void changeDateInFile(vector<Task>& taskVector, string date, int priority, string text, string newDate);
-void changeTextInFile(vector<Task>& taskVector, string date, int priority, string text, string newText);
-void deleteTaskInFile(vector<Task>& taskVector, string date, int priority, string text);
+void changePriorityInFile(std::vector<Task>& taskVector, std::string date, int priority, 
+std::string text, int newPriority);
+
+void changeStringInFile(std::vector<Task>& taskVector, std::string date_p, int priority_p, 
+std::string text_p, std::string newString, int typeString);
+
+void changeDateInFile(std::vector<Task>& taskVector, std::string date, int priority, 
+std::string text, std::string newDate);
+
+void changeTextInFile(std::vector<Task>& taskVector, std::string date, int priority, 
+std::string text, std::string newText);
+
+void deleteTaskInFile(std::vector<Task>& taskVector, std::string date, int priority, 
+std::string text);
 
