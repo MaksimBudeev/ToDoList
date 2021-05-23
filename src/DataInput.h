8e78c7a14529c65b6447be8721c309fabc16e61d
Meghan-Murray <aa.mironets@gmail.com>
2021-05-24 01:53:59 +0700

    Changing the type of work with a file. Reading from a vector.

--- src/DataInput.h
+++ src/DataInput.h
@@ -4,23 +4,14 @@
 #include <iterator>
 #include <string>
 #include <vector>
+#include <sstream>
 #include "Task.h"
 
-void writeInVector(std::vector<Task>& taskVector);
-void setTaskInFile(std::vector<Task>& taskVector, std::string date, int priority, std::string text);
+enum class Operation{
+	Read,
+	Edit,
+	Delete,
+	Add
+};
 
-void changePriorityInFile(std::vector<Task>& taskVector, std::string date, int priority, 
-std::string text, int newPriority);
-
-void changeStringInFile(std::vector<Task>& taskVector, std::string date_p, int priority_p, 
-std::string text_p, std::string newString, int typeString);
-
-void changeDateInFile(std::vector<Task>& taskVector, std::string date, int priority, 
-std::string text, std::string newDate);
-
-void changeTextInFile(std::vector<Task>& taskVector, std::string date, int priority, 
-std::string text, std::string newText);
-
-void deleteTaskInFile(std::vector<Task>& taskVector, std::string date, int priority, 
-std::string text);
-
+void changeStringInFile(std::vector<Task>& taskVector, size_t index, Operation type);
